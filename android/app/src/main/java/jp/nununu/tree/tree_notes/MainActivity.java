e6efb359e29aa281206c2fe84aa6930c46148eb4
nnn-uchisawa <53939263+nnn-uchisawa@users.noreply.github.com>
2025-10-06 07:58:55 +0900

    Android設定とリソースの更新
    - Gradle設定ファイルの更新（build.gradle, settings.gradle）
    - AndroidManifest.xmlの設定変更
    - アプリケーションアイコンの更新
    - ランチ背景の設定変更
    - スタイルとテーマの更新
    - フレーバー設定の追加（dev, local, prod）
    - Kotlinファイルの削除とJavaファイルの追加

---
+++ android/app/src/main/java/jp/nununu/tree/tree_notes/MainActivity.java
@@ -0,0 +1,6 @@
+package jp.nununu.tree.tree_notes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/example/tree_memo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tree_memo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
